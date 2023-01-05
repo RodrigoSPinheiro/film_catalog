af52e62328c227aa4520871ea850887a55734664
RodrigoSPinheiro <rodrigo13329@gmail.com>
2023-01-05 02:51:40 -0400

    add repository

--- src/controllers/FilmController.ts
+++ src/controllers/FilmController.ts
@@ -1,7 +1,33 @@
 import { Request, Response } from "express";
+import { filmRepository } from "../repositories/filmRepository";
 
 export class FilmController {
     async create(req: Request, res: Response){
-        return res.json('cheguei no controlador')
+        const {title, author, description} = req.body
+
+        if(!title){
+            return res.status(400).json({ message: 'O titulo é obrigatório'})
+        }
+
+        if(!author){
+            return res.status(400).json({ message: 'O autor é obrigatório'})
+        }
+
+        if(!description){
+            return res.status(400).json({ message: 'O descrição é obrigatório'})
+        }
+
+        try {
+                const newFilm = filmRepository.create({ title, author, description })
+
+                await filmRepository.save(newFilm)
+
+                return res.status(201).json(newFilm)
+
+                
+        } catch (error){
+            console.log(error);
+            return res.status(500).json ({ message: 'Internal Server Error'})
+        }
     }
 }
